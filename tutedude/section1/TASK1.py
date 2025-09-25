73bf908ee88831f0560a6204f435283b911e3966
jayantanand24 <jayant.cse24@gmail.com>
2025-09-25 15:39:33 +0530

    commit message

---
+++ tutedude/section1/TASK1.py
@@ -0,0 +1,7 @@
+a=int(input('enter the 1st number: '))
+b=int(input('enter the 2nd number: '))
+
+print('addition: ', a+b)
+print('subtraction: ', a-b)
+print('multiplication: ', a*b)
+print('division: ', a/b)
