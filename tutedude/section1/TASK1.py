44b8f04b3ce1034bd58f8df0f82a2fbbb8af1181
jayantanand24 <151828374+jayantanand24@users.noreply.github.com>
2025-09-25 15:42:47 +0530

    Delete tutedude directory

--- tutedude/section1/TASK1.py
+++
@@ -1,7 +0,0 @@
-a=int(input('enter the 1st number: '))
-b=int(input('enter the 2nd number: '))
-
-print('addition: ', a+b)
-print('subtraction: ', a-b)
-print('multiplication: ', a*b)
-print('division: ', a/b)
--- tutedude/section1/TASK2.py
+++
@@ -1,4 +0,0 @@
-a=str(input('Enter your first name: '))
-b=str(input('Enter your last name: '))
-
-print('Hello, ' + a + ' ' + b +'! Welcome to python programming.' )
